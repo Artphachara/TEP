50e4778be7aeeb044c76ed4215799f1477e457dc
Phachara <f@Fs-MacBook-Pro.local>
2023-08-09 10:38:07 +0700

    develop: update file for install library

---
+++ setup.py
@@ -0,0 +1,70 @@
+###########
+# Imports #
+###############################################################################
+
+import os
+
+from setuptools import find_packages, setup
+
+#############
+# Variables #
+###############################################################################
+
+package_name: str = 'TEP'
+
+version: str = '1.0.0'
+
+description: str = '''
+
+'''
+
+author: str = '\n'.join([
+    'Phachara Wuttisetpaiboon',
+])
+
+packages: list[str] = find_packages(exclude=['tests', 'tests.*',])
+
+requirements_path = os.path.join('requirements.txt')
+
+#############
+# Functions #
+###############################################################################
+
+
+def read_requirements(path: str) -> list[str]:
+    """
+    Read a requirements text file as a list of dependencies.
+
+    Parameters
+    ----------
+    path : str
+        Path to the requirements text file.
+
+    Returns
+    -------
+    list[str]
+        List of dependencies.
+    """
+    with open(path, 'r') as file:
+        reqs_list: list[str] = file.read().strip().split('\n')
+
+    return reqs_list
+
+########
+# Main #
+###############################################################################
+
+requirements: list[str] = read_requirements(requirements_path)
+
+setup(
+    name=package_name,
+    version=version,
+    description=description,
+    author=author,
+    packages=packages,
+    python_requires='>=3.9.0',
+    install_requires=requirements,
+    include_package_data = True,
+)
+
+###############################################################################
--- translator/translate/_translate.py
+++ translator/translate/_translate.py
@@ -14,7 +14,7 @@ import tqdm
 
 class Translate:
     """
-
+    This is class of main translator centre.
     """
 
     ###########
@@ -23,14 +23,27 @@ class Translate:
 
     def thai_to_eng(dataframe: pd.DataFrame) -> pd.DataFrame:
         """
-
+        Translate Thai text to English using Google Translate.
+        
+        Parameters: 
+        -----------
+        dataframe: DataFrame
+            A DataFrame containing a column 'Thai' with Thai text.
+        
+        Returns: DataFrame
+        --------
+            The input DataFrame with an added
+            'English'column containing translated text.
         """
+
         translator = Translator()
         
         english_data = []
 
         for thai_text in tqdm.tqdm(dataframe['Thai'], desc="Translating"):
-            english_text = translator.translate(thai_text, src='th', dest='en').text
+            english_text = translator.translate(
+                thai_text, src='th', dest='en'
+            ).text
             english_data.append(english_text)
 
         dataframe['English'] = english_data
